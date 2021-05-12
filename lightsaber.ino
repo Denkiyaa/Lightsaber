74ebea389b1429c7136ba164a003ccb412ec83f6
Berkay <84093840+Denkiyaa@users.noreply.github.com>
2021-05-12 20:33:59 +0300

    Add files via upload

---
+++ lightsaber.ino
@@ -0,0 +1,169 @@
+#include <Wire.h>
+#include <ADXL345.h>
+#include <SD.h>                      // need to include the SD library
+#define SD_ChipSelectPin 4  //using digital pin 4 on arduino nano 328
+#include <TMRpcm.h>           //  also need to include this library...
+#include <SPI.h>
+
+ADXL345 adxl; //variable adxl is an instance of the ADXL345 library
+TMRpcm tmrpcm;   // create an object for use in this sketch
+int buttonPushCounter = 0;   // counter for the number of button presses
+int buttonState = 0;         // current state of the button
+int lastButtonState = 0;     // previous state of the button
+
+void setup(){
+  Serial.begin(9600);
+  adxl.powerOn();
+
+  //set activity/ inactivity thresholds (0-255)
+  adxl.setActivityThreshold(75); //62.5mg per increment
+  adxl.setInactivityThreshold(75); //62.5mg per increment
+  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look of activity movement on this axes - 1 == on; 0 == off 
+  adxl.setActivityX(1);
+  adxl.setActivityY(1);
+  adxl.setActivityZ(1);
+ 
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  adxl.setInactivityX(1);
+  adxl.setInactivityY(1);
+  adxl.setInactivityZ(1);
+ 
+  //look of tap movement on this axes - 1 == on; 0 == off
+  adxl.setTapDetectionOnX(0);
+  adxl.setTapDetectionOnY(0);
+  adxl.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  adxl.setTapThreshold(250); //62.5mg per increment
+  adxl.setTapDuration(15); //625μs per increment
+  adxl.setDoubleTapLatency(80); //1.25ms per increment
+  adxl.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //set values for what is considered freefall (0-255)
+  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //setting all interupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interupt actions - 1 == on; 0 == off  
+  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+  
+  tmrpcm.speakerPin = 9;
+  if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+  return;   // don't do anything more if not
+  }
+  
+  tmrpcm.quality(1);
+  pinMode(5,OUTPUT);
+}
+
+
+void loop(){
+
+ 
+
+  //Boring accelerometer stuff   
+  int x,y,z;  
+  adxl.readAccel(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+
+  // Output x,y,z values - Commented out
+  //Serial.print(x);
+  //Serial.print(y);
+  //Serial.println(z);
+
+
+  //Fun Stuff!    
+  //read interrupts source and look for triggerd actions
+  
+  //getInterruptSource clears all triggered actions after returning value
+  //so do not call again until you need to recheck for triggered actions
+   byte interrupts = adxl.getInterruptSource();
+  
+  // freefall
+  if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
+    Serial.println("freefall");
+    //add code here to do when freefall is sensed
+  } 
+  
+  //inactivity
+  if(adxl.triggered(interrupts, ADXL345_INACTIVITY)){
+    Serial.println("inactivity");
+    tmrpcm.play("3.wav");
+    delay(150);
+     //add code here to do when inactivity is sensed
+  }
+  
+  //activity
+  if(adxl.triggered(interrupts, ADXL345_ACTIVITY)){
+    Serial.println("activity"); 
+    tmrpcm.play("4.wav");
+    delay(700);
+    
+     //add code here to do when activity is sensed
+  }
+  delay(250);
+  
+  //double tap
+  if(adxl.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+    Serial.println("double tap");
+    delay(100);
+     //add code here to do when a 2X tap is sensed
+  }
+  
+  //tap
+  if(adxl.triggered(interrupts, ADXL345_SINGLE_TAP)){
+    Serial.println("tap");
+    tmrpcm.play("5.wav");
+    delay(300);
+  } 
+
+ 
+   
+   buttonState = digitalRead(3);
+
+  
+  if (buttonState != lastButtonState) {
+   
+    if (buttonState == HIGH) {
+     
+      buttonPushCounter++;
+        Serial.println(buttonPushCounter);
+    } else {
+    }
+    delay(50);
+  }
+ 
+  lastButtonState = buttonState;
+
+  if (buttonPushCounter % 2 == 1) {
+    
+    if (buttonState == HIGH) {
+      tmrpcm.pause();
+            
+         digitalWrite(5,HIGH); 
+         tmrpcm.play("1.wav");
+         delay(1300);
+         
+         }
+    }
+  
+   else if (buttonState == HIGH){
+     digitalWrite(5,LOW);
+    tmrpcm.play("2.wav");
+    return;
+  }
+  
+
+}
